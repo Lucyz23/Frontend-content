8dd8caa2c9f884fe415f9f56942effcac2676660
Lucyz23 <lucymagalhaesbarbosa@gmail.com>
2023-03-31 00:05:58 -0300

    Projeto Frontend content

---
+++ src/Content.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const conteudos = [
+  {
+    nome: 'JavaScript assíncrono',
+    bloco: 9,
+    status: 'já aprendi',
+  },
+  {
+    nome: 'Composição de Componentes',
+    bloco: 10,
+    status: 'estou aprendendo',
+  },
+  {
+    nome: 'Composição de Estados',
+    bloco: 11,
+    status: 'aprenderei',
+  },
+  {
+    nome: 'Redux',
+    bloco: 15,
+    status: 'aprenderei',
+  },
+];
+
+class Content extends React.Component {
+  render() {
+    return (
+      <ul>
+        {conteudos.map(({ nome, bloco, status }, index) => (
+          <li key={ index }>
+            {`Eu ${status} o 
+                    conteúdo ${nome} no bloco ${bloco}`}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default Content;
